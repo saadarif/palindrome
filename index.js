188d4242d9b3c8c64662503229d55515a8c9e12c
Saad Arif <arifsaad@gmail.com>
2023-04-26 21:48:09 +0100

    Add test for empty string

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ function Phrase(content) {
     return Array.from(this.content).filter(c => c.match(lettersRegEx)).join("");
   }
 
-  // Returns true if the phrase is a palindrome, false otherwise.
+    // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+
+    } else {
+      return false;
+    }
   }
 }
 
